6ec7f802fe7a2f8d00612d208cf56bbc49d2354f
AleksaRadovanovic <aleksa.radovanovich@gmail.com>
2022-03-20 18:10:31 +0100

    set colors

--- src/constants/color-constants.js
+++ src/constants/color-constants.js
@@ -1,8 +1,8 @@
 const Colors = {
-    PRIMARY: "",
-    SECONDARY: "",
-    LIGHT: "",
-    DARK: "",
+    PRIMARY: "#30475E",
+    SECONDARY: "#F05454",
+    LIGHT: "#F5F5F5",
+    DARK: "#121212",
 }
 
 export default {
